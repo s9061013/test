2fd387e79f69e6d2befcee751724d6e75372d067
s9061013 <s9061013@gmail.com>
2020-03-14 15:24:57 +0800

    master version1

---
+++ HelloEclipse.java
@@ -0,0 +1,8 @@
+package xxx;
+
+public class HelloEclipse {
+	public static void main(String[] args) {//main方法
+		System.out.println("jsfj;sd");
+		                                                                  //run configuration - arguments -輸入值
+	}
+}
