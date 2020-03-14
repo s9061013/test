aa42f402e52563de8def7f75b8f3eb615bb81fcc
s9061013 <s9061013@gmail.com>
2020-03-14 15:30:52 +0800

    master version1

--- HelloEclipse.java
+++ HelloEclipse.java
@@ -3,6 +3,7 @@ package xxx;
 public class HelloEclipse {
 	public static void main(String[] args) {//main方法
 		System.out.println("hello eclipse");
+		String a ="hello world";
 		                                                                  //run configuration - arguments -輸入值
 	}
 }
