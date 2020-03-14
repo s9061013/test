0e50015e2d668a7a3ad61d95f3820956d2f0a9c7
s9061013 <s9061013@gmail.com>
2020-03-14 15:27:37 +0800

    branch version1

--- HelloEclipse.java
+++ HelloEclipse.java
@@ -2,7 +2,7 @@ package xxx;
 
 public class HelloEclipse {
 	public static void main(String[] args) {//main方法
-		System.out.println("jsfj;sd");
+		System.out.println("hello eclipse");
 		                                                                  //run configuration - arguments -輸入值
 	}
 }
